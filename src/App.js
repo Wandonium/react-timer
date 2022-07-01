da4df2e19355766d3077a5918c6ac1daac7c16ec
Wandonium <hillarywando@gmail.com>
2022-07-01 22:14:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [error, setError] = useState('');
+  const [timer, setTimer] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if(timer) {
+      interval = setInterval(() => {
+        if(seconds <= 0) {
+          setMinutes(minutes - 1);
+          setSeconds(60);
+        } else {
+          setSeconds(seconds - 1)
+        }
+      }, 1000);
+
+      if(minutes < 0) {
+        setMinutes(0);
+        setSeconds(0);
+        setTimer(false);
+      }
+    }
+    return () => clearInterval(interval);
+  });
+
+
+  const handleMinChange = (event) => {
+    let min = parseInt(event.target.value);
+    console.log('min: ', min);
+    if(min < 0 || min > 60) {
+      setError('Please enter a valid number of minutes');
+    } else {
+      setMinutes(min);
+    }
+  }
+
+  const handleSecChange = event => {
+    let sec = parseInt(event.target.value);
+    console.log('sec: ', sec);
+    if(sec < 0 || sec > 60) {
+      setError('Please enter a valid number of seconds');
+    } else {
+      setSeconds(sec);
+    }
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // console.log('minutes: ', minutes);
+    // console.log('seconds: ', seconds);
+    setTimer(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        error !== '' ? (
+          <div>
+            {error}
+          </div>
+        ) : (
+  
+          <div>
+            <h1>{minutes}:{seconds}</h1>
+            <input type="number" id="minutes" placeholder="Enter minutes" onChange={handleMinChange}/>
+            <br /><br />
+            <input type="number" id="seconds" placeholder="Enter seconds" onChange={handleSecChange}/>
+            <br /><br />
+            <button onClick={handleSubmit}>Submit</button>
+          </div>
+        )
+      }
     </div>
   );
 }
